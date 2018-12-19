d6a92864b54e3cadf1c23d2c211fcb97c9a1a2f7
Karin Ljunggren <kaljunggren@gmail.com>
2018-12-19 19:47:39 +0100

    home section

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,90 @@ const Home = () => {
       <div className="container">
         <div className="row">
           <div className="col-12">
-            <h1>Home</h1>
+            <h2 className="heading">
+              <span>See our work</span>
+            </h2>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-12">
+            <div className="img-block-row">
+              <div className="img-block-wrapper">
+                <div className="img-block">
+                  <a href="/">
+                    <div
+                      className="img-block-element"
+                      style={{
+                        backgroundImage:
+                          "url(https://itera-cdn.azureedge.net/globalassets/all-images/itera-people-to-grow.jpg)"
+                      }}
+                    />
+                  </a>
+                  <div className="block-text">
+                    <a href="/">
+                      <h3>People to grow with</h3>
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="img-block-wrapper">
+                <div className="img-block">
+                  <a href="/">
+                    <div
+                      className="img-block-element"
+                      style={{
+                        backgroundImage:
+                          "url(https://itera-cdn.azureedge.net/globalassets/all-images/itera-academy.jpg)"
+                      }}
+                    />
+                  </a>
+                  <div className="block-text">
+                    <a href="/">
+                      <h3>Itera Academy</h3>
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="img-block-wrapper">
+                <div className="img-block">
+                  <a href="/">
+                    <div
+                      className="img-block-element"
+                      style={{
+                        backgroundImage:
+                          "url(https://itera-cdn.azureedge.net/globalassets/all-images/itera-csr.jpg)"
+                      }}
+                    />
+                  </a>
+                  <div className="block-text">
+                    <a href="/">
+                      <h3>Top 5 most innovative</h3>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="img-block-wrapper">
+                <div className="img-block">
+                  <a href="/">
+                    <div
+                      className="img-block-element"
+                      style={{
+                        backgroundImage:
+                          "url(https://itera-cdn.azureedge.net/globalassets/all-images/m0a8630.jpg)"
+                      }}
+                    />
+                  </a>
+                  <div className="block-text">
+                    <a href="/">
+                      <h3>Awards</h3>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
